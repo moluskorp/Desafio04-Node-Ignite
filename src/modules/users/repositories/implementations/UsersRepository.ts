ef8cc7e8e70ee86c47788719fec773b9b2ab528d
Junior Baratella <molusko.rp@hotmail.com>
2021-03-21 15:09:04 -0300

    Desafio Finalizado

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,65 @@
+import { User } from "../../model/User";
+import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private users: User[];
+
+  private static INSTANCE: UsersRepository;
+
+  private constructor() {
+    this.users = [];
+  }
+
+  public static getInstance(): UsersRepository {
+    if (!UsersRepository.INSTANCE) {
+      UsersRepository.INSTANCE = new UsersRepository();
+    }
+
+    return UsersRepository.INSTANCE;
+  }
+
+  create({ name, email }: ICreateUserDTO): User {
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+      admin: false,
+    });
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  findById(id: string): User | undefined {
+    return this.users.find((user) => user.id === id);
+  }
+
+  findByEmail(email: string): User | undefined {
+    return this.users.find((user) => user.email === email);
+  }
+
+  turnAdmin(receivedUser: User): User {
+    const user = { ...receivedUser, admin: true };
+
+    const usersAltered = this.users.map((userMap) => {
+      if (userMap.id === user.id) {
+        return user;
+      }
+      return userMap;
+    });
+
+    this.users = usersAltered;
+
+    return user;
+  }
+
+  list(): User[] {
+    return this.users;
+  }
+}
+
+export { UsersRepository };
---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,28 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const existEmail = this.usersRepository.findByEmail(email);
+
+    if (existEmail) {
+      throw new Error("Email already exists");
+    }
+
+    const user = this.usersRepository.create({
+      name,
+      email,
+    });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
